00bd1b45e23fc21cfebdcacf35309fc068dbbc8a
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-09-14 18:02:48 +0100

    top section description

--- src/app/containers/Homepage/topSection.tsx
+++ src/app/containers/Homepage/topSection.tsx
@@ -17,7 +17,51 @@ const TopSectionContainer = styled.div`
 
 `;
 
+const LeftContainer = styled.div`
+    ${tw`
+        flex
+        flex-col
+        w- 1/2
+    `};
+`;
 
+const RightContainer = styled.div`
+    ${tw`
+        flex
+        flex-col
+        w-1/2
+        relative
+        mt-20
+    `};
+`;
+
+const Slogan = styled.h1`
+    ${tw`
+        font-bold
+        text-2xl
+        xl:text-6xl
+        md:text-5xl
+        sm:text-3xl
+        lg:font-black
+        md:font-extrabold
+        text: black
+        mb-4
+        sm:leading-smug
+        lg:leading-normal
+        xl:leading-relaxed
+    `};
+`;
+
+const Description = styled.p`
+    ${tw`
+        text-xs
+        lg:text-sm
+        xl:text-lg
+        sm:max-h-full
+        overflow:hidden
+          
+    `};
+`;
 export function TopSection(){
 
 
